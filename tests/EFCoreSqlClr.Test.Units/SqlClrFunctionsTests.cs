94ef557b2bf8fad6d7a78a9460ca3464833cfd64
Zeid Youssefzadeh <zeidlabco@gmail.com>
2025-05-16 13:09:20 +0330

    Add unit tests for SQL CLR functions and update project references

---
+++ tests/EFCoreSqlClr.Test.Units/SqlClrFunctionsTests.cs
@@ -0,0 +1,11 @@
+namespace ZeidLab.ToolBox.EasyPersistence.EFCoreSqlClr.Test.Units
+{
+    public class SqlClrFunctionsTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
